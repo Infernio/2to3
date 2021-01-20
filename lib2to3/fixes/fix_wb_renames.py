bc3bc0d4a0ca9fbf342c3b4a3e204e3b92f14aab
Infernio <infernio@icloud.com>
2021-01-20 23:37:09 +0100

    Add fixer for unsafe renames
    'Unsafe' because they will probably cause false positives in other
    codebases, but they are totally safe in WB's codebase.

---
+++ lib2to3/fixes/fix_wb_renames.py
@@ -0,0 +1,17 @@
+"""Fixes certain APIs that were changed. I ensured this will have no false
+positives in WB's codebase."""
+
+from ..pgen2 import token
+from .. import fixer_base
+
+_remaps = {'getheaders': 'get'}
+
+class FixWbRenames(fixer_base.BaseFix):
+    BM_compatible = True
+    PATTERN = "'getheaders'"
+
+    def transform(self, node, results):
+        if node.type == token.NAME:
+            new = node.clone()
+            new.value = _remaps[node.value]
+            return new
